b408ac6823827995a084e2b1fb01fde7724cc39e
Christopher Ancheta <christopherancheta@Christophers-MacBook-Pro.local>
2025-07-30 21:45:19 +1200

    Fixed nav gap

--- funeralsite/settings.py
+++ funeralsite/settings.py
@@ -84,27 +84,40 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'Pacific/Auckland'
+
 USE_I18N = True
-USE_TZ = True
+
+USE_TZ = False
+
 
 # Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'funeralsite/static'),  # Point to your app's static directory
 ]
 
-# Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Crispy Forms
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+#Messagse
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'success',
+    messages.ERROR: 'danger'
 
-# Email settings (for production)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'noreply@funeralservices.com'
+}
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
